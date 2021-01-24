aa32f7b5dc20fd5030ca97c7407b08a209e791fb
Jon Lauridsen <mail@jonlauridsen.com>
2021-01-24 16:23:08 +0100

    examples + test-automation

---
+++ examples/ts/valid/someExamples.ts
@@ -0,0 +1,10 @@
+import os from "os"
+import path from "path"
+
+console.log("hello world")
+
+const a = ["a"].map(el => el)
+
+function func (): string { return "foo" }
+
+console.log(os, path, a, func)
